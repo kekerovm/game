1279fb01459754684f167049bc93e17c65d7edb9
Michel Bronco Kekerovic <michelpunchcode@gmail.com>
2020-01-27 15:49:55 -0800

    game

---
+++ game.js
@@ -0,0 +1,12 @@
+alert("Welcome To VOLVO 920980!")
+
+var driver = confirm("Are you the driver?")
+
+if (driver) {
+  alert("Welcome, Driver!")
+  var name = prompt("What is your name?")
+  alert(`Hi, ${name}. Please be safe today.`)
+} else {
+  alert("You are NOT the driver.")
+}
+// if(name == 2327){}
