00447c931f6215d2e5423c110829945d29cd0c87
Michel Bronco Kekerovic <michelpunchcode@gmail.com>
2020-01-28 08:37:53 -0800

    game

--- game.js
+++ game.js
@@ -5,8 +5,30 @@ var driver = confirm("Are you the driver?")
 if (driver) {
   alert("Welcome, Driver!")
   var name = prompt("What is your name?")
-  alert(`Hi, ${name}. Please be safe today.`)
+  alert(`Hi, ${name}.`)
+  var id = prompt("Please enter your driver id")
+  if (
+    id == `9402` ||
+    id == `3518` ||
+    id == `9527` ||
+    id == `2469` ||
+    id == `7834`
+  ) {
+    alert("Thank you. Please be safe today")
+    // } else {
+    //   alert("You are not Authorized!")
+  }
 } else {
-  alert("You are NOT the driver.")
+  alert("You are NOT the driver. Goodbye")
 }
-// if(name == 2327){}
+
+// (function() {
+// 	var input 	= "",
+// 		correct = "1593";
+
+// var str=DriverIdNumber = ("9402", "3518", "9527", "2469", "7834")
+// if (matcher.matches) () {
+//   System.out.println(DriverIdNumber + " = Thank you. Please be safe today!") :
+// } else
+//   System.out.println(DriverIdNumber + " = Please call Dispatch"):
+// }
